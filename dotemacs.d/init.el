c4c5b148a8ffd99f94c6f405f6320a41c8844115
realrealkoc@gmail.com <kc.pavlov@gmail.com>
2016-01-12 12:51:39 +0300

    work state

--- dotemacs.d/init.el
+++ dotemacs.d/init.el
@@ -101,6 +101,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
 (require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete/dict")
+(ac-config-default)
 
 
 (add-to-list 'load-path "~/.emacs.d/plugins/ergoemacs-mode")
@@ -109,15 +111,3 @@
 (setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
 (setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
 (ergoemacs-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(python-shell-interpreter "python2"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
